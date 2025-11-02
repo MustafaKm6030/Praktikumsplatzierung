aec881ff42ce7185480522759eb93d8e41942c01
zara <afreenzara26@gmail.com>
2025-11-02 21:27:34 +0100

    #1 Update Layout component with structure and styling

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -4,15 +4,17 @@ import Header from './Header';
 import './Layout.css';
 
 function Layout({ children }) {
-    return (
-        <div className="layout">
-        <Sidebar />
-        <div className="layout-main">
+return (
+    <div className="layout">
+    <Sidebar />
+    <div className="layout-main">
         <Header />
         <main className="layout-content">
-            {children}
-            </main>
-        </div>
+        {children}
+        </main>
     </div>
-    );
+    </div>
+);
 }
+
+export default Layout;
