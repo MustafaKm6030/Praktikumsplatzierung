f3e991167dfbf96479ebe048207e1f58fb35c02c
Muhammad Malik <malik08@ads.uni-passau.de>
2025-12-02 18:30:59 +0000

    Implementation 24

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/backend-static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Additional locations of static files
